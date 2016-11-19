2f2c3f8eded12dc03f756884cfcd83c506437996
byunghyunpark <openmind8735@gmail.com>
2016-11-19 20:28:51 +0900

    nested relationshi을 사용하여 post list 안의 comment_list 리팩토링함
    * commentSerializer를 postserializer 위로 올림

--- django_app/post/serializers.py
+++ django_app/post/serializers.py
@@ -3,21 +3,29 @@ from rest_framework import serializers
 from post.models import Post, Comment
 
 
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-    # to_representation으로 photo_list에 comment_list 추가
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        ret['comment_list'] = CommentSerializer(instance.comment_set.all(), many=True).data
-        return ret
-
-
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    # neasted relationship을 사용하여 comment를 참조함
+    # read only 안하면 def create를 따로 선언해야함(보통 read only)
+    comment_list = CommentSerializer(many=True, read_only=True, source='comment_set')
+
+    class Meta:
+        model = Post
+        fields = (
+            'id',
+            'author',
+            'content',
+            'comment_list'
+        )
+
+    # to_representation으로 photo_list에 comment_list 추가
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        # ret['comment_list'] = CommentSerializer(instance.comment_set.all(), many=True).data
+        return ret
