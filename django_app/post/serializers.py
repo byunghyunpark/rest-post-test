b8105b6abe31e837aaee4589ce3f8551c2b35e25
byunghyunpark <openmind8735@gmail.com>
2016-11-19 19:56:27 +0900

    photo list에 commet list 추가
    * PhotoSerializer에서 to_representation 메소드 override
    * CommentSerializer을 상속 받아서 serializer된 comment를 Photo_list에 넣음

--- django_app/post/serializers.py
+++ django_app/post/serializers.py
@@ -4,9 +4,16 @@ from post.models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Post
         fields = '__all__'
+
+    # to_representation으로 photo_list에 comment_list 추가
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['comment_list'] = CommentSerializer(instance.comment_set.all(), many=True).data
+        return ret
 
 
 class CommentSerializer(serializers.ModelSerializer):
